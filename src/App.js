e4b00e5e3c85792e13046651ceb52b165b59b8bb
Kevin Dial <kevin.dial.phone@gmail.com>
2016-12-16 00:52:14 -0500

    [chore] variable for number of lines

--- src/App.js
+++ src/App.js
@@ -22,14 +22,12 @@ class App extends Component {
     console.log("did mount");
   }
   onDrop(acceptedFiles, rejectedFiles, context) {
+    const num_lines = 40;
     const chatFile = acceptedFiles[0];
     const reader = new FileReader();
     reader.onload = function(event) {
       const lines = event.target.result.split('\n');
-      this.setState({ messages: lines.slice(0, 10) });
-      for(let i = 0; i < 10; i++){
-        console.log(lines[i]);
-      }
+      this.setState({ messages: lines.slice(0, num_lines) });
       this.renderMessages();
     }.bind(context);
 
